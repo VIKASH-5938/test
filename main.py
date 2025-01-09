2ecd15afc7b1d06973ef823a3d2f9c569f6c4fdd
VIKASH-5938 <134520842+VIKASH-5938@users.noreply.github.com>
2025-01-09 19:45:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def start_keep_alive_thread():
 # Start keep-alive in a separate thread
 Thread(target=start_keep_alive_thread, daemon=True).start()
 
-TOKEN = '7068418588:AAFiPaXOOZHsUNCH2I7O22vQZg5LoPmsRQE'
+TOKEN = ''
 MONGO_URI = 'mongodb+srv://VIKASH:BadnamBadshah@cluster0.jv9he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
 FORWARD_CHANNEL_ID = -1002226609740
 CHANNEL_ID = -1002226609740
